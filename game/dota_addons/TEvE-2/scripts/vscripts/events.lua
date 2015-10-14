e06119fa8e52d221eccb583e618cc5b4d846ed79
Samuel Cattini-Schultz <catts16@gmail.com>
2015-10-14 21:06:02 +1100

    Started work on game logic and changed directory format

---
+++ game/dota_addons/TEvE-2/scripts/vscripts/events.lua
@@ -0,0 +1,343 @@
+-- This file contains all teve-registered events and has already set up the passed-in parameters for your use.
+-- Do not remove the TEvE:_Function calls in these events as it will mess with the internal teve systems.
+
+-- Cleanup a player when they leave
+function TEvE:OnDisconnect(keys)
+  DebugPrint('[TEVE] Player Disconnected ' .. tostring(keys.userid))
+  DebugPrintTable(keys)
+
+  local name = keys.name
+  local networkid = keys.networkid
+  local reason = keys.reason
+  local userid = keys.userid
+
+end
+-- The overall game state has changed
+function TEvE:OnGameRulesStateChange(keys)
+  DebugPrint("[TEVE] GameRules State Changed")
+  DebugPrintTable(keys)
+
+  -- This internal handling is used to set up main teve functions
+  TEvE:_OnGameRulesStateChange(keys)
+
+  local newState = GameRules:State_Get()
+end
+
+-- An NPC has spawned somewhere in game.  This includes heroes
+function TEvE:OnNPCSpawned(keys)
+  DebugPrint("[TEVE] NPC Spawned")
+  DebugPrintTable(keys)
+
+  -- This internal handling is used to set up main teve functions
+  TEvE:_OnNPCSpawned(keys)
+
+  local npc = EntIndexToHScript(keys.entindex)
+end
+
+-- An entity somewhere has been hurt.  This event fires very often with many units so don't do too many expensive
+-- operations here
+function TEvE:OnEntityHurt(keys)
+  --DebugPrint("[TEVE] Entity Hurt")
+  --DebugPrintTable(keys)
+
+  local damagebits = keys.damagebits -- This might always be 0 and therefore useless
+  if keys.entindex_attacker ~= nil and keys.entindex_killed ~= nil then
+    local entCause = EntIndexToHScript(keys.entindex_attacker)
+    local entVictim = EntIndexToHScript(keys.entindex_killed)
+
+    -- The ability/item used to damage, or nil if not damaged by an item/ability
+    local damagingAbility = nil
+
+    if keys.entindex_inflictor ~= nil then
+      damagingAbility = EntIndexToHScript( keys.entindex_inflictor )
+    end
+  end
+end
+
+-- An item was picked up off the ground
+function TEvE:OnItemPickedUp(keys)
+  DebugPrint( '[TEVE] OnItemPickedUp' )
+  DebugPrintTable(keys)
+
+  local heroEntity = EntIndexToHScript(keys.HeroEntityIndex)
+  local itemEntity = EntIndexToHScript(keys.ItemEntityIndex)
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local itemname = keys.itemname
+end
+
+-- A player has reconnected to the game.  This function can be used to repaint Player-based particles or change
+-- state as necessary
+function TEvE:OnPlayerReconnect(keys)
+  DebugPrint( '[TEVE] OnPlayerReconnect' )
+  DebugPrintTable(keys) 
+end
+
+-- An item was purchased by a player
+function TEvE:OnItemPurchased( keys )
+  DebugPrint( '[TEVE] OnItemPurchased' )
+  DebugPrintTable(keys)
+
+  -- The playerID of the hero who is buying something
+  local plyID = keys.PlayerID
+  if not plyID then return end
+
+  -- The name of the item purchased
+  local itemName = keys.itemname 
+  
+  -- The cost of the item purchased
+  local itemcost = keys.itemcost
+  
+end
+
+-- An ability was used by a player
+function TEvE:OnAbilityUsed(keys)
+  DebugPrint('[TEVE] AbilityUsed')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local abilityname = keys.abilityname
+end
+
+-- A non-player entity (necro-book, chen creep, etc) used an ability
+function TEvE:OnNonPlayerUsedAbility(keys)
+  DebugPrint('[TEVE] OnNonPlayerUsedAbility')
+  DebugPrintTable(keys)
+
+  local abilityname=  keys.abilityname
+end
+
+-- A player changed their name
+function TEvE:OnPlayerChangedName(keys)
+  DebugPrint('[TEVE] OnPlayerChangedName')
+  DebugPrintTable(keys)
+
+  local newName = keys.newname
+  local oldName = keys.oldName
+end
+
+-- A player leveled up an ability
+function TEvE:OnPlayerLearnedAbility( keys)
+  DebugPrint('[TEVE] OnPlayerLearnedAbility')
+  DebugPrintTable(keys)
+
+  local player = EntIndexToHScript(keys.player)
+  local abilityname = keys.abilityname
+end
+
+-- A channelled ability finished by either completing or being interrupted
+function TEvE:OnAbilityChannelFinished(keys)
+  DebugPrint('[TEVE] OnAbilityChannelFinished')
+  DebugPrintTable(keys)
+
+  local abilityname = keys.abilityname
+  local interrupted = keys.interrupted == 1
+end
+
+-- A player leveled up
+function TEvE:OnPlayerLevelUp(keys)
+  DebugPrint('[TEVE] OnPlayerLevelUp')
+  DebugPrintTable(keys)
+
+  local player = EntIndexToHScript(keys.player)
+  local level = keys.level
+end
+
+-- A player last hit a creep, a tower, or a hero
+function TEvE:OnLastHit(keys)
+  DebugPrint('[TEVE] OnLastHit')
+  DebugPrintTable(keys)
+
+  local isFirstBlood = keys.FirstBlood == 1
+  local isHeroKill = keys.HeroKill == 1
+  local isTowerKill = keys.TowerKill == 1
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local killedEnt = EntIndexToHScript(keys.EntKilled)
+end
+
+-- A tree was cut down by tango, quelling blade, etc
+function TEvE:OnTreeCut(keys)
+  DebugPrint('[TEVE] OnTreeCut')
+  DebugPrintTable(keys)
+
+  local treeX = keys.tree_x
+  local treeY = keys.tree_y
+end
+
+-- A rune was activated by a player
+function TEvE:OnRuneActivated (keys)
+  DebugPrint('[TEVE] OnRuneActivated')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local rune = keys.rune
+
+  --[[ Rune Can be one of the following types
+  DOTA_RUNE_DOUBLEDAMAGE
+  DOTA_RUNE_HASTE
+  DOTA_RUNE_HAUNTED
+  DOTA_RUNE_ILLUSION
+  DOTA_RUNE_INVISIBILITY
+  DOTA_RUNE_BOUNTY
+  DOTA_RUNE_MYSTERY
+  DOTA_RUNE_RAPIER
+  DOTA_RUNE_REGENERATION
+  DOTA_RUNE_SPOOKY
+  DOTA_RUNE_TURBO
+  ]]
+end
+
+-- A player took damage from a tower
+function TEvE:OnPlayerTakeTowerDamage(keys)
+  DebugPrint('[TEVE] OnPlayerTakeTowerDamage')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local damage = keys.damage
+end
+
+-- A player picked a hero
+function TEvE:OnPlayerPickHero(keys)
+  DebugPrint('[TEVE] OnPlayerPickHero')
+  DebugPrintTable(keys)
+
+  local heroClass = keys.hero
+  local heroEntity = EntIndexToHScript(keys.heroindex)
+  local player = EntIndexToHScript(keys.player)
+end
+
+-- A player killed another player in a multi-team context
+function TEvE:OnTeamKillCredit(keys)
+  DebugPrint('[TEVE] OnTeamKillCredit')
+  DebugPrintTable(keys)
+
+  local killerPlayer = PlayerResource:GetPlayer(keys.killer_userid)
+  local victimPlayer = PlayerResource:GetPlayer(keys.victim_userid)
+  local numKills = keys.herokills
+  local killerTeamNumber = keys.teamnumber
+end
+
+-- An entity died
+function TEvE:OnEntityKilled( keys )
+  DebugPrint( '[TEVE] OnEntityKilled Called' )
+  DebugPrintTable( keys )
+
+  TEvE:_OnEntityKilled( keys )
+  
+
+  -- The Unit that was Killed
+  local killedUnit = EntIndexToHScript( keys.entindex_killed )
+  -- The Killing entity
+  local killerEntity = nil
+
+  if keys.entindex_attacker ~= nil then
+    killerEntity = EntIndexToHScript( keys.entindex_attacker )
+  end
+
+  -- The ability/item used to kill, or nil if not killed by an item/ability
+  local killerAbility = nil
+
+  if keys.entindex_inflictor ~= nil then
+    killerAbility = EntIndexToHScript( keys.entindex_inflictor )
+  end
+
+  local damagebits = keys.damagebits -- This might always be 0 and therefore useless
+
+  -- Put code here to handle when an entity gets killed
+end
+
+
+
+-- This function is called 1 to 2 times as the player connects initially but before they 
+-- have completely connected
+function TEvE:PlayerConnect(keys)
+  DebugPrint('[TEVE] PlayerConnect')
+  DebugPrintTable(keys)
+end
+
+-- This function is called once when the player fully connects and becomes "Ready" during Loading
+function TEvE:OnConnectFull(keys)
+  DebugPrint('[TEVE] OnConnectFull')
+  DebugPrintTable(keys)
+
+  TEvE:_OnConnectFull(keys)
+  
+  local entIndex = keys.index+1
+  -- The Player entity of the joining user
+  local ply = EntIndexToHScript(entIndex)
+  
+  -- The Player ID of the joining player
+  local playerID = ply:GetPlayerID()
+end
+
+-- This function is called whenever illusions are created and tells you which was/is the original entity
+function TEvE:OnIllusionsCreated(keys)
+  DebugPrint('[TEVE] OnIllusionsCreated')
+  DebugPrintTable(keys)
+
+  local originalEntity = EntIndexToHScript(keys.original_entindex)
+end
+
+-- This function is called whenever an item is combined to create a new item
+function TEvE:OnItemCombined(keys)
+  DebugPrint('[TEVE] OnItemCombined')
+  DebugPrintTable(keys)
+
+  -- The playerID of the hero who is buying something
+  local plyID = keys.PlayerID
+  if not plyID then return end
+  local player = PlayerResource:GetPlayer(plyID)
+
+  -- The name of the item purchased
+  local itemName = keys.itemname 
+  
+  -- The cost of the item purchased
+  local itemcost = keys.itemcost
+end
+
+-- This function is called whenever an ability begins its PhaseStart phase (but before it is actually cast)
+function TEvE:OnAbilityCastBegins(keys)
+  DebugPrint('[TEVE] OnAbilityCastBegins')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.PlayerID)
+  local abilityName = keys.abilityname
+end
+
+-- This function is called whenever a tower is killed
+function TEvE:OnTowerKill(keys)
+  DebugPrint('[TEVE] OnTowerKill')
+  DebugPrintTable(keys)
+
+  local gold = keys.gold
+  local killerPlayer = PlayerResource:GetPlayer(keys.killer_userid)
+  local team = keys.teamnumber
+end
+
+-- This function is called whenever a player changes there custom team selection during Game Setup 
+function TEvE:OnPlayerSelectedCustomTeam(keys)
+  DebugPrint('[TEVE] OnPlayerSelectedCustomTeam')
+  DebugPrintTable(keys)
+
+  local player = PlayerResource:GetPlayer(keys.player_id)
+  local success = (keys.success == 1)
+  local team = keys.team_id
+end
+
+-- This function is called whenever an NPC reaches its goal position/target
+function TEvE:OnNPCGoalReached(keys)
+  DebugPrint('[TEVE] OnNPCGoalReached')
+  DebugPrintTable(keys)
+
+  local goalEntity = EntIndexToHScript(keys.goal_entindex)
+  local nextGoalEntity = EntIndexToHScript(keys.next_goal_entindex)
+  local npc = EntIndexToHScript(keys.npc_entindex)
+end
+
+-- This function is called whenever any player sends a chat message to team or All
+function TEvE:OnPlayerChat(keys)
+  local teamonly = keys.teamonly
+  local userID = keys.userid
+  local playerID = self.vUserIds[userID]:GetPlayerID()
+
+  local text = keys.text
+end
--- scripts/vscripts/addon_game_mode.lua
+++
@@ -1,4 +0,0 @@
-print("Hello, my name is addon_game_mode, and I am a gamemode 15 initializer")
-
-local TEvE = TEvEGameMode:new()
-TEvE.InitGameMode()
